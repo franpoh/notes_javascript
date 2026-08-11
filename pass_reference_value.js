fbcd99a337b1000a2d361f8068742a7cc5630099
Fran Poh <franjpoh@gmail.com>
2026-08-11 14:45:07 +0800

    Pushing study notes

--- pass_reference_value.js
+++ pass_reference_value.js
@@ -1,139 +1,147 @@
 
-// You learn about 'pass by value' and 'pass by reference', but
-// NOTE: JavaScript is always pass-by-value. 
+// You learn about 'pass by value' and 'pass by reference'
+
+// NOTE:Pass by Value: the data is copied and anything you do to it inside the function will only persist in the function scope
+
+// NOTE: Pass by Reference: you are given a pointer/handle/reference to the data, and it refers to the same underlying data as the original scope. 
+// So if you modify it, that can be seen outside the scope. 
+
+
+
+// NOTE: BUT JavaScript is always pass-by-value. 
 // This means everything in JavaScript is a value type and function arguments are always passed by value. 
-// That being said, object types are a bit more confusing.
-
-
-
-// ----------------------------- > OBJECTS -----------------------------
-
-// for objects the value of the variable is a reference
-// Because of this, when you pass an object and change its members 
-// those changes persist outside of the function.
-
-// This makes it look like pass by reference. 
-// But if you actually change the value of the object variable 
-// you will see that the change does not persist, proving it's really pass by value.
-
-// NOTE: The value that is passed is the reference 'address' to the 'real' object
-// You need to access the 'real' object located at the reference 'address' to make any changes
-// You can't just make changes to the reference 'address' and expect anything to happen to the 'real' object
-
-
-
-// A good analogy will be a contractor visiting a house for painting
-
-const house1 = { wall: "white" };
-const house2 = { wall: "white" };
-
-function paintWall (address1, address2) { // We supply our contractor with the addresses of the houses he has to paint
-
-    // contractor goes to the house at address1 to paint the wall
-    // he goes to the address, gets into the house, and paint the wall pink
-
-    address1.wall = 'pink'; // pointing directly to the member in the object that address1 is referencing with its value
-
-    // contractor is literally saying address2 now has a house with a wall that is blue
-    // but he doesn't goes to address2 or access the house at address2 at all
-    // he's just stating address2 now has some random ass house with a blue wall
-    // in fact, because of this, address2 now is an entirely different address to a house the contractor made up
-
-    address2 = { wall: 'blue' }; // pointing to value of address2, which is a reference, and replacing it with another object entirely
-
-}
-
-paintWall(house1, house2);
-
-console.log(house1); // { wall: 'pink' }
-console.log(house2); // { wall: 'white' }
-
-// Simply put, changes to the object inside the function will affect the original object, as they both refer to the same object. 
-// However, reassigning the value of the variable holding the object originally will not affect the object referenced by the function.
-// NOTE: In short, if you make changes to the reference only, nothing will happen
-
-
-
-// The term pass by reference and pass by value only applies to function calls and their arguments. 
-
-// NOTE: The lesser used and known term that was coined is Call by sharing which applies to Ruby, JavaScript, Python, Java and so forth. 
-// It implies that all values are object, all values are boxed, and they copy a reference when they pass it as value.
-// In super simple terms, "Call by sharing" means that you are handing the function a duplicate address to your house, not the house itself.
-
-// Here is what that means in practice:
-// Imagine an Object in JavaScript is a house, and a variable is an address to that house.
-// When you pass an Object into a function, JavaScript makes a copy of your adress and gives it to the function. 
-// Now both you and the function have noted down the address to the exact same house.
-
-
-
-// +++++ Another Example
-
-// Changing the object reference in the function
-
-function PassbyReference(obj) {
-    obj = {
-        a: 10,
-        b: 20,
-        c: 30,
-    }
-    console.log("Hello from inside the function: ", obj);
-}
-
-let objA = {
-    a: 10,
-    b: 20
-}
-
-console.log("Before calling Pass By Reference Function: ", objA); // Before calling Pass By Reference Function:  { a: 10, b: 20 }
-PassbyReference(objA); // Hello from inside the function:  { a: 10, b: 20, c: 30 }
-console.log("After calling Pass By Reference Function:", objA); // After calling Pass By Reference Function: { a: 10, b: 20 }
-
-
-
-// Changing the original object
-
-function PassbyReference(obj) {
-    obj.c = 30;
-    console.log("Hello from inside the function: ", obj);
-}
-
-let objB = {
-    a: 10,
-    b: 20
-}
-
-console.log("Before calling Pass By Reference Function: ", objB); // Before calling Pass By Reference Function:  { a: 10, b: 20 }
-PassbyReference(objB); // Hello from inside the function:  { a: 10, b: 20, c: 30 }
-console.log("After calling Pass By Reference Function:", objB); // After calling Pass By Reference Function: { a: 10, b: 20, c: 30 }
 
 
 
 // ----------------------------- > PRIMITIVES -----------------------------
 
-// Function is called by directly passing the value of the variable as the argument. 
-// Changing the argument inside the function doesn’t affect the variable passed from outside the function.
+// Let's take a look at passing primitives into a function to better understand how passing objects into a function works
+// In this case, the value of the variable that we are about to pass into the function is a primitive, hence 'passing primitives'
 
+// Remember, we are passing by value: the data is copied and anything you do to the data inside the function will only affect it within the function scope
 
 let num = 10;
 
-function callByValue(no) {
-    // We can see very obviously here that only the value of the variable was passed in, not the variable itself
-    console.log(no); // 10
-    no = 100;
+function passPrimitiveValue(no) { 
+    console.log(`Before attempting change: ${no}`);
+    no = 100; // we attempt to change the value that was passed into the function here
+    console.log(`After attempting change: ${no}`); 
 }
 
-callByValue(num);
+passPrimitiveValue(num); // we call the function by passing the value of the variable 'num' into the function
+// Before attempting change: 10 - here, you can see it is merely a value - a copy of the value of 'num'
+// After attempting change: 100 - here, you can see the value has been changed, but as you will later see, only within the function scope
 
-console.log("num: ", num); // num: 10 - no change occurs
+console.log("num: ", num); 
+// num: 10 - no change occurs with the value of 'num' in the global scope, hence proving that what we passed into the function is just a copy
 
 
 
-// NOTE: Javascript always pass by value so changing the value of the variable never changes the original primitive 
-
-
+// It works differently if you change the value of a variable directly and not within a function
 
 let cyp = 10;
 
 cyp = 100; // directly changing the value of a variable
 console.log(`cyp: ${cyp}`); // cyp: 100
+
+
+
+// ----------------------------- > OBJECTS -----------------------------
+
+// +++++ Is passing objects into a function really pass by reference? Here is an example:
+
+let passingObject = {changeThis: 'unchanged'};
+
+function passObjectFunction (passedObject) {
+    console.log(`Before changing passedObject's changeThis: ${passedObject.changeThis}`);
+
+    passedObject.changeThis = 'changed'; // we attempt to change a member in the object that was passed into the function here
+
+    console.log(`After changing passedObject's changeThis: ${passedObject.changeThis}`);
+}
+
+passObjectFunction(passingObject); // we call the function by passing the value of the variable 'passingObject' into the function
+// Before changing passedObject's changeThis: unchanged
+// After changing passedObject's changeThis: changed - here, you can see the value has been changed in the function scope
+
+console.log(`passingObject's changeThis: ${passingObject.changeThis}`);
+// passingObject's changeThis: changed - it has been changed in the original variable in global scope as well
+
+
+
+// Remember, pass by reference: you are given a pointer/handle/reference to the data, and it refers to the same underlying data as the original scope. 
+// So if you modify it, that can be seen outside the scope - as it demonstrated in the above example
+
+// So this example makes it look like pass by reference - but it isn't
+
+
+
+// When the value of a variable is an object, that value is actually a reference - it is pointing to the actual value stored somewhere in memory
+// variable = {object} - the variable points to its value, and the value points to an object in memory
+
+// +++++ Here is an example that demonstrates how the value points to an object in memory
+
+passingObject = {changeThis: 'unchanged'};
+let copyObject = passingObject; // copyObject's value now points to passingObject's object as well
+
+function passObjectFunction (passedObject) {
+    console.log(`Before changing passedObject's changeThis: ${passedObject.changeThis}`);
+
+    passedObject.changeThis = 'changed'; // we attempt to change a member in the object that was passed into the function here
+
+    console.log(`After changing passedObject's changeThis: ${passedObject.changeThis}`);
+}
+
+passObjectFunction(passingObject); 
+// Before changing passedObject's changeThis: unchanged
+// After changing passedObject's changeThis: changed
+
+console.log(`passingObject's changeThis: ${passingObject.changeThis}`); // passingObject's changeThis: changed
+console.log(`copyObject's changeThis: ${copyObject.changeThis}`); // copyObject's changeThis: changed
+
+// As you can see in the above example, both passingObject and copyObject were changed both in the function scope and global scope, even though we only appear to have changed the member of passingObject
+// This demonstrates how the value of both the passingObject and copyObject variable were pointing at the same object 
+
+
+
+// Now that we have demonstrated how the variable's value points to an object in memory
+// We can say that the value of that variable is a reference (to the object in memory) - but nonetheless, it is still a value
+// Thus we say that Javascript is always Pass by Value
+
+// +++++ Here is example that demonstrates that even though passing an object into a function looks like pass by reference
+// it is in fact pass by value - the data is copied and anything you do to it inside the function will only persist in the function scope
+
+let passObjectWrong = {changeThis: 'unchanged'};
+let passObjectCorrect = {changeThis: 'unchanged'};
+
+function passObjectFunction (passObjectWrong, passObjectCorrect) { // passing in the copied values of whichever variables we call this function with
+    console.log(`Before changing passObjectWrong's changeThis: ${passObjectWrong.changeThis}`);
+    console.log(`Before changing passObjectCorrect's changeThis: ${passObjectCorrect.changeThis}`);
+
+    // we attempt to change a member in the object that was passed into the function here - note the difference between the two
+    passObjectWrong = {changeThis: 'changed'}; // changing the copied value 
+    passObjectCorrect.changeThis = 'changed'; // going to the member of the object that the copied value is pointing at, and changing it
+
+    console.log(`After changing passObjectWrong's changeThis: ${passObjectWrong.changeThis}`);
+    console.log(`After changing passObjectCorrect's changeThis: ${passObjectCorrect.changeThis}`);
+}
+
+passObjectFunction(passObjectWrong, passObjectCorrect); // here, we can see that both objects were changed in the function scope
+/*
+Before changing passObjectWrong's changeThis: unchanged
+Before changing passObjectCorrect's changeThis: unchanged
+
+After changing passObjectWrong's changeThis: changed
+After changing passObjectCorrect's changeThis: changed
+*/ 
+
+console.log(`passObjectWrong's changeThis: ${passObjectWrong.changeThis}`);
+// passObjectWrong's changeThis: unchanged - it remains unchanged in the original variable in global scope - hence proving what we changed was merely a copied value
+
+console.log(`passObjectCorrect's changeThis: ${passObjectCorrect.changeThis}`);
+//passObjectCorrect's changeThis: changed - it has been changed in the original variable in global scope as well
+
+
+
+// NOTE: The lesser used and known term that was coined is Call by sharing which applies to Ruby, JavaScript, Python, Java and so forth. 
+// It implies that all values are object, all values are boxed, and they copy a reference when they pass it as value.
